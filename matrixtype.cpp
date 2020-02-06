fa62362f76561e50c356b3d891ae156e96cd4146
Braulio Mac <mac_hip3r@macs-mac.local>
2020-02-06 17:11:59 -0600

    Funcion para determinar si es reflectiva

--- matrixtype.cpp
+++ matrixtype.cpp
@@ -7,61 +7,97 @@ using namespace std;
 int matrix[256][256];
 
 // Prototypes
-void aihaReadMatrix(int mat[][256], int m, int n) {
+int aihaReadMatrix(int mat[][256], int m) {
 	int i = 0, j = 0;
 
 	//Inizialiting loop. Get the elements from the user.
 	for(i = 0; i < m; i++) {
-		for(j = 0; j < n; j++ ) {
+		for(j = 0; j < m; j++ ) {
 			cout << "Input the [" << i+1 << "][" << j+1 << "] element: ";
 			cin >> mat[i][j];
 		}
 	}
+
+    return 0;
 }
-void aihaPrintMatrix(int mat[][256], int m, int n) {
+
+int aihaPrintMatrix(int mat[][256], int m) {
 	int i = 0, j = 0;
 
 	cout << "The matrix: " << endl << endl;
 
 	//This are the nested loops that prints the matrix on screen with format (all elements are inside of [] so it looks more like a real matrix).
 	for (i = 0; i < m; i++) {
-		cout << setw(21) << "[";
+		cout << "\t[";
 
-		for (j = 0; j < n; j++) {
-			if(j != n-1)
+		for (j = 0; j < m; j++) {
+			if(j != (m - 1))
 				cout << mat[i][j] << setw(12);
 			else cout << mat[i][j];
 		}
-		cout << "]" << endl;
+
+		cout << "]\n" << endl;
 	}
+
+    return 0;
 }
-void reflectiveMatrix(float matrix[]);
-void symmetricMatrix(float matrix[]);
-void nonSymmetricMatrix(float matrix[]);
-void transitiveMatrix(float matrix[]);
+
+int bmReflectiveMatrix(int matrix[][256], int m){
+    int counter = 0;
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < m; j++) {
+            if(i == j)
+                if(matrix[i][j] == 1) {
+                    counter++;
+                }
+        }
+    }
+
+    if(counter == m) cout << "\n\nThe matrix is REFLECTIVE" << endl;
+    else cout << "\n\nThe matrix is NOT REFLECTIVE" << endl;
+
+    return 0;
+}
+
+int symmetricMatrix(int matrix[][256], int m){
+
+
+    return 0;
+}
+
+int nonSymmetricMatrix(int matrix[][256], int m){
+
+
+    return 0;
+}
+
+int transitiveMatrix(int matrix[][256], int m){
+
+
+    return 0;
+}
 
 int main() {
     // Declarations
-	int m = 0, n = 0;
+	int m = 0;
 
     // Welcome
-    cout << "\nProgram to determine what type of array is entered. \n\tDiscrete Mathematics.";
+    cout << "\nProgram to determine what type of array is entered. \n\n\tDiscrete Mathematics.\n\n";
 
     // Get matrix from user
 	cout << "What's the matrix dimension? " << endl;
 	cout << "m: ";
 	cin >> m;
-	cout << "n: ";
-	cin >> n;
-    aihaReadMatrix(matrix, m, n);
+    aihaReadMatrix(matrix, m);
     // Print Matrix
-    aihaPrintMatrix(matrix, m, n);
+    aihaPrintMatrix(matrix, m);
 
     // Matrix Types
-    reflectiveMatrix(matrix);
-    symmetricMatrix(matrix);
+    bmReflectiveMatrix(matrix, m);
+    /*symmetricMatrix(matrix);
     nonSymmetricMatrix(matrix);
-    transitiveMatrix(matrix);
+    transitiveMatrix(matrix);*/
 
     return 0;
 }
