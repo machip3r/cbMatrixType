8fc78a1a7460dd29f935189917e8c7966e4be423
Braulio Mac <mac_hip3r@macs-mac.local>
2020-02-06 16:19:20 -0600

    Funciones y comentarios

--- matrixtype.cpp
+++ matrixtype.cpp
@@ -1,9 +1,30 @@
+// Preprocessor directives
 #include <iostream>
 
+// Global Declarations
 using namespace std;
 
-int main() {
+// Prototypes
+float getMatrix();
+void reflectiveMatrix(float matrix[]);
+void symmetricMatrix(float matrix[]);
+void nonSymmetricMatrix(float matrix[]);
+void transitiveMatrix(float matrix[]);
 
+int main() {
+    // Declarations
+    float matrix[3][3];
+
+    // Welcome
+    cout << "\nPrograma para determinar qué tipo de matriz es la que se introduce. \n\tMatemáticas Discretas.";
+
+    // Get matrix from user
+    getMatrix();
+
+    reflectiveMatrix(matrix);
+    symmetricMatrix(matrix);
+    nonSymmetricMatrix(matrix);
+    transitiveMatrix(matrix);
 
     return 0;
 }
